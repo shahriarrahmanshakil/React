de252a1abdb6ab2d9888f6dedfe3bae90cf6c8c3
shahriarrahmanshakil <shahriarrahmanshakil@gmail.com>
2024-06-18 00:27:56 +0600

    second-react-project added

---
+++ Learn-React-From-Anisul_islam/Tutorial 3  Create first React project with Vite/second-react-project/src/index.js
@@ -0,0 +1,9 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+
+const root = document.getElementById('root')
+ReactDOM.createRoot(root).render(
+    <div>
+          <h1>Hello world</h1>
+    </div>
+)
