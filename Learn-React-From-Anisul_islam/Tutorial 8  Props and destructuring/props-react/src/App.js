a97c0a97495a4b81d9e53be955cf75057f388d7c
shahriarrahmanshakil <shahriarrahmanshakil@gmail.com>
2024-06-19 09:49:27 +0600

    Props and Destucturing added

--- Learn-React-From-Anisul_islam/Tutorial 8  Props and destructuring/props-react/src/App.js
+++ Learn-React-From-Anisul_islam/Tutorial 8  Props and destructuring/props-react/src/App.js
@@ -4,10 +4,12 @@ import Card from "./components/Card";
 function App(){
     return  <div>
                 <h1 className="headingStyle">To-Do App</h1>
-                <Card />
-                <Card />
-                <Card />
-                <Card />
+                <Card title ="This is title 1" Description = "This is Description - 1"/>
+                <Card title ="This is title 2" Description = "This is Description - 2"/>
+                <Card title ="This is title 3" Description = "This is Description - 3"/>
+                <Card title ="This is title 4" Description = "This is Description - 4"/>
+                <Card title ="This is title 5" Description = "This is Description - 5"/>
+                <Card title ="This is title 6" Description = "This is Description - 6"/>
             </div>
 }
 export default App;
--- Learn-React-From-Anisul_islam/Tutorial 8  Props and destructuring/props-react/src/components/Card.js
+++ Learn-React-From-Anisul_islam/Tutorial 8  Props and destructuring/props-react/src/components/Card.js
@@ -1,9 +1,9 @@
-const title = "This is Title";
-const Description  = "This is Description";
+
 const date = new Date();
 let fulldate = date.getDate() + "/" + date.getMonth() + "/" + date.getFullYear();
 
-function Card(){
+function Card(props){
+  const {title,Description} = props
   return  <div className='card'>
             <p className='title'>{title}</p>
             <p className='Descripttion'>{Description}</p>
