fe1317a06ded8638005f421d06d181389fcb1098
shahriarrahmanshakil <shahriarrahmanshakil@gmail.com>
2024-06-21 22:35:40 +0600

    react-project-02 added

---
+++ Learn-React-From-Anisul_islam/Tutorial 3  create react project with CRA  react 18/react-project-02/src/App.js
@@ -0,0 +1,11 @@
+
+  function App() {
+    return (
+      <div>
+        <h1>React js</h1>
+        <p>React lets you build user interfaces out of individual pieces called components. Create your own React components like Thumbnail, LikeButton, and Video. Then combine them into entire screens, pages, and apps.</p>
+      </div>
+    );
+  }
+
+  export default App;
