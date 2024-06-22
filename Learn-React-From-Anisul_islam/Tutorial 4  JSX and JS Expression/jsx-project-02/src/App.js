fe42bbe26bb3818383151e2a9b5fe50792298161
shahriarrahmanshakil <shahriarrahmanshakil@gmail.com>
2024-06-22 13:11:56 +0600

    jsx-project-02 upgraded

--- Learn-React-From-Anisul_islam/Tutorial 4  JSX and JS Expression/jsx-project-02/src/App.js
+++ Learn-React-From-Anisul_islam/Tutorial 4  JSX and JS Expression/jsx-project-02/src/App.js
@@ -1,10 +1,17 @@
+const react = "Learning React js";
+const js = "React JS is JavaScript Libary";
+const fw = "React js is javascript frontned framework";
+const date = new Date();
+const fullDate = date.getDate() + "/" + date.getMonth() + "/" + date.getFullYear(); 
 
 
 function App() {
   return (
     <div>
-      <h1>Learning React js</h1>
-      <p>React js is javaScript Libary</p>
+      <h1>{react}</h1>
+      <p>{js}</p>
+      <p>{fw}</p>
+      <strong>{fullDate}</strong>
     </div>
   );
 }
