ac2838b188a4476253b39cff3ed5a3851cf220f3
shahriarrahmanshakil <shahriarrahmanshakil@gmail.com>
2024-06-22 14:36:46 +0600

    css style added

---
+++ Learn-React-From-Anisul_islam/Tutorial 5  CSS Styling in JSX/css-jsx-project-02/src/App.js
@@ -0,0 +1,21 @@
+
+
+
+const mystyle = {
+  textAlign : "center",
+  backgroundColor : "orange",
+  fontSize : "3rem",
+  color : "black",
+  padding : "20px"
+}
+
+function App() {
+  return (
+    <div>
+      <h1 style={mystyle}>Bangladesh</h1>
+      <p className="pstyle">Bangladesh, officially the People's Republic of Bangladesh, is a country in South Asia. It is the eighth-most populous country in the world and is among the most densely populated countries with a population of nearly 170 million in an area of 148,460 square kilometres 57,320 sq mi. Bangladesh shares land borders with India to the north, west, and east, and Myanmar to the southeast. To the south, it has a coastline along the Bay of Bengal. It is narrowly separated from Bhutan and Nepal by the Siliguri Corridor, and from China by the mountainous Indian state of Sikkim in the north. Dhaka, the capital and largest city, is the nation's political, financial, and cultural centre. Chittagong is the second-largest city and is the busiest port on the Bay of Bengal. The official language of Bangladesh is Bengali while Bangladeshi English is also used in the government and official documents alongside Bengali.</p>
+    </div>
+  );
+}
+
+export default App;
