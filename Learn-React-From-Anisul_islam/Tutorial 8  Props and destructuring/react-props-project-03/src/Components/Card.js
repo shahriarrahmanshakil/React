bdac3cb6e557fd23d3a862ad6c637bf52257859c
shahriarrahmanshakil <shahriarrahmanshakil@gmail.com>
2024-06-23 11:04:55 +0600

    props and destructuring added

---
+++ Learn-React-From-Anisul_islam/Tutorial 8  Props and destructuring/react-props-project-03/src/Components/Card.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+
+let date = new Date();
+let fullDate = date.getDate() + "/" + date.getMonth() + "/" + date.getFullYear(); 
+
+function Card(props){
+    console.log(props);
+    let {title,Description} = props;
+    return <div className="card">
+    <h3 className="cardtitle">{title}</h3>
+    <p className="cardbody">{Description}</p>
+    <p className="carddate">{fullDate}</p>
+    </div>
+}
+export default Card;
