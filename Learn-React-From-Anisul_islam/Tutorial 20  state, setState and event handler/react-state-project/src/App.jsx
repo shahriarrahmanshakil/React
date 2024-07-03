32e1861cf6c84bdecdbaa36bbc97edc572ddf70e
shahriarrahmanshakil <shahriarrahmanshakil@gmail.com>
2024-07-03 11:29:43 +0600

    react state added

---
+++ Learn-React-From-Anisul_islam/Tutorial 20  state, setState and event handler/react-state-project/src/App.jsx
@@ -0,0 +1,11 @@
+import STATE from "./STATE";
+
+function App() {
+  return (
+    <>
+      <h1>Hello World</h1>
+      <STATE count = "0" />
+    </>
+  )
+}
+export default App
--- Learn-React-From-Anisul_islam/Tutorial 20  state, setState and event handler/react-state-project/src/STATE.jsx
+++ Learn-React-From-Anisul_islam/Tutorial 20  state, setState and event handler/react-state-project/src/STATE.jsx
@@ -1,14 +1,35 @@
-import React,{ Component } from "react";
+import React,{Component} from 'react'
 
-class STATE extends Component{
-    render(){
-        return(
-        <div>
-            <h1>Hello</h1>
-        </div>
-        )
+class STATE extends Component {
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         count : 1
+      }
+    }
+    Decriment = () => {
+       this.setState({
+            count : this.state.count - 1
+       })
     }
 
+    Incriment = () => {
+        this.setState({
+            count : this.state.count + 1
+       })
+    }
+    
+    render(){
+        const {count} = this.state
+        return(
+            <div>
+                Count : {count}
+                <br />
+                <button onClick={this.Decriment} disabled={count == 0}> - </button>
+                <button onClick={this.Incriment}>+</button>
+            </div>
+        )
+    }
 }
 export default STATE;
-
