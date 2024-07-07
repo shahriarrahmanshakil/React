ed08bf0dbda3b4ecf6ea9d8e8657987507c2478d
shahriarrahmanshakil <shahriarrahmanshakil@gmail.com>
2024-07-07 23:51:20 +0600

    class-component-project-02 added

---
+++ Learn-React-From-Anisul_islam/Tutorial 15  class component/class-component-project-02/src/App.js
@@ -0,0 +1,13 @@
+import Card1 from "./components/Card1";
+import Card2 from "./components/Card2";
+
+function App() {
+  return (
+    <>
+      <Card1 name = "This is Card-01" description = "This is Card-01 Description" />
+      <Card2 name = "This is Card-02" description = "This is Card-02 Description" />
+    </>
+  );
+}
+
+export default App;
