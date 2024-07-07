3a2f28df0e971d33b0faca7075d4d79a6ea7648b
shahriarrahmanshakil <shahriarrahmanshakil@gmail.com>
2024-07-07 09:02:58 +0600

    second-react-project

---
+++ Learn-React-From-Anisul_islam/Tutorial 3  create react project with CRA  react 18/second-react-project/src/App.js
@@ -0,0 +1,14 @@
+
+
+function App() {
+  return (
+        <>
+          <h1>Hello World</h1>
+          <h1>Hello Bangladesh</h1>
+          <code>This is Shahriar Rahman Shakil</code>
+          <p>This is second-react-project</p>
+        </>
+  );
+}
+
+export default App;
